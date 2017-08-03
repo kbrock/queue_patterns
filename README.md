ddb79cf19e6d3a9b5e9fcd90d61ecae437a6e17b
Keenan Brock <keenan@thebrocks.net>
2017-08-03 08:46:21 -0400

    Add example 8 - coordinator with filter info
    see also
    https://solinea.com/blog/tapping-kubernetes-events

--- README.md
+++ README.md
@@ -1,31 +1,55 @@
 # Purpose
 
 We currently have a producer consumer paradigm.
-In this process, we sometimes generate work requests that are duplicates.
+This repo focuses on the orchestration of the work
+it punts on the queue, database, and spawner.
 
 This is from the context of our C&U processing.
-But `put_unless_exists` is common place and removing duplicate requests is pretty common.
+We are using `put_unless_exists` to make sure we don't ask for the same work to be done multiple times
 
 
-This could setup a database, use ActiveMQ, and use Redis, but this implementation attempted
-to simplify everything.
+# Text Diagram
+
+```
+coordinator/producer:
+  every 3 minutes:
+    get all possible objects (~cheap)
+    determine threshold for each object: (expensive)
+      if alerts enabled, threshold is every 10 minutes
+      if no alerting, threshold is every 50 minutes
+    keep objects where last_perf_capture_on is over threshold
+    submit objects => queue 
+
+collector/consumer:
+  while (get message from queue)
+    fetch object from db
+    fetch data from provider
+    update last_perf_capture_on
+```
 
 # The Problem
 
 The first pass through needs to submit work for every record.
 The timer goes off before all records are submitted.
-
-When trying to detect which records need work to be done, a bunch of false positivies show up.
-These are then inserted again.
+The second pass through generates a bunch of requests for records already submitted (the last_perf_capture_on is not yet set)
 
 
 So... How do you not do the same work twice?
+How can you reduce duplicates, without using `put_unless_exists` on the queue - it will no longer be available.
+How do you determine how much work needs to be done?
+Why are we even using a timer with arbitray values?
+Can we put processing optimizations in the collector not the coordinator
 
-Sure, you could increase the number of workers, or you could change the way you determine what work needs to be done.
 
-Our goal here is to accept that duplicates may be requested, and instead reduce the amount of work done
+Do note, that increasing the number of workers will fix this problem. But there is a generalized problem of needing to
+request work that others are also requesting.
 
-dedup examples:
+Our goal here is to reduce the number of duplicates generated.
+
+# current examples
+
+There are a number of approaches to reducing the duplicates.
+Starting with example 6, the definition of a producer and consumer has changed:
 
 1. calculate work - do work (inline single)
 2. calculate work - spawn work (wait till they are done)
@@ -33,14 +57,33 @@ dedup examples:
 4. filter on client (time where requests will be blocked)
 5. filter on server (same algorithm as 4)
 6. fixed collectors
+7. fixed collectors with supervisor that sets filter
+8. fixed collector with supervisor that coordinates filters (http / events)
 ---
 ?. queue for collectors, queue for writers
+?. collector just processing alerts or non-alert objects (sleep full 10min or 50min)
 ?. queue params separate from actual (queue need refresh, vms to refresh in a separate set)
 ?. "shopping list" a queue consumers (for outside interface), more db like queue for work.
 
-problem set:
-currently handle occasonal duplicate records (so capture / delay / broadcast not viable)
-what about many many duplicates
-what about running 2 producers ("oops")
-what about work that takes a long time
-what about sending deletes, and retries?
+
+
+# "Fixed collectors" Diagram
+
+```
+Supervisor
+  knows each collector's filter
+  publishes filters as configuration map or HTTP
+  receives events for new/deleted collectors and adjusts filters
+
+Collector 1:
+  filter = "objects.id mod 2 == 0"
+  loop:
+    fetch filter changes (or use events)
+    fetch objects using filter
+      hit provider
+      save metrics
+      provide_scale_information
+      sleep(nice)
+Collector 2:
+  filter = "objects.id mod 2 == 1"
+```
---
+++ dedup08.rb
@@ -0,0 +1,182 @@
+#!/usr/bin/env ruby
+require 'thread'
+require_relative 'common'
+
+puts "worker dynamic workload http_filters"
+
+START = Time.now
+COLLECTOR_COUNT = 2   # number of collectors
+RECORD_COUNT    = 100 # number of records in database
+INTERVAL        = 2   # frequency of determining if there needs to be work
+DURATION        = 20  # length of test
+DELAY           = 0.1 # time each work item takes
+PADDING         = 3 # assume task runs every 3 (or takes 3)
+
+class Supervisor < WorkerBase
+  # populated from descriptor
+  attr_accessor :count
+  def initialize
+    super("S", nil, [])
+    @filters = {}
+    @mutex = Mutex.new
+  end
+
+  # event in k8s
+  def collector_added(id)
+    @filters[id] = ""
+    assign_filters
+    @filters[id]
+  end
+
+  # event in k8s
+  def collector_removed(id)
+    @filters.delete(id)
+    assign_filters
+  end
+
+  # http request
+  # could use config maps instead
+  def get_filter(id)
+    @filters[id]
+  end
+
+  # http request
+  def finished_work(id, delta = nil)
+    if delta && delta < -0.01
+      print_with_time "OVER BY #{"%.3f" % -delta}"
+    end
+    # print SPACER
+    # feedback mechanism
+    # determine if we need more / less
+  end
+
+  def count=(count)
+    @count = count
+    assign_filters
+  end
+
+  def assign_filters
+    # need a better algorithm
+    # choose non nil entries
+    # attempt to produce same filter for same id if possible
+    # not sure if we should set a nil to a filter (ask it to go away)
+    #   or if that is the job for someone else
+    @filters.keys.each_with_index do |id, i|
+      if i <  count
+        @filters[id] = "#{count}%#{i}"
+      else # have too many clients, ask one to go away?
+        @filters[id] = nil
+      end
+    end
+  end
+end
+
+class Collector < WorkerBase
+  attr_accessor :supervisor
+  def initialize(my_n, db, s)
+    super(my_n, db, [])
+    @supervisor = s
+  end
+
+  def filter=(value)
+    @filter = text_to_block(value)
+  end
+
+  def please_shutdown
+    @filter = nil
+  end
+
+  def done?
+    @filter.nil?
+  end
+
+  def process_mine
+    @db.select(&@filter).each do |rec|
+      if (t = rec.status)
+        process(rec)
+        print t
+        @processed += 1
+      end
+    end
+    true
+  end
+
+  # basically Collector#run
+  def run
+    supervisor.collector_added(@my_n)
+    print_with_time("START")
+    # run_loop(nil, INTERVAL) do
+    #   self.filter = supervisor.get_filter(@my_n)
+    #   break if done?
+    #   process_mine
+    # end
+    run_nice(INTERVAL, supervisor) do
+      self.filter = supervisor.get_filter(@my_n)
+      !done? && process_mine
+    end
+    supervisor.collector_removed(@my_n)
+  end
+
+  def process(record) # record is passed in
+    sleep DELAY
+    @db[record.id] = record.touch # save
+  end
+
+  # filter: "mod%val"
+  # future filter may include ems_id and object types
+  def text_to_block(txt)
+    if txt.nil?
+      nil
+    elsif txt.include?("%")
+      mod, val = txt.split("%")
+      mod = mod.to_i
+      val = val.to_i
+      Proc.new { |r| r.id % mod == val }
+    else
+      raise "bad filter"
+    end
+  end
+
+  # to be extracted
+  def run_nice(interval, boss)
+    loop do
+      start = Time.now
+      old_sz = @q.size
+      print_with_time("WORK")
+      has_more = yield
+      new_sz = @q.size
+      if new_sz != old_sz
+        print " q: #{old_sz}=>#{new_sz}\n#{SPACER}"
+      else
+        print "\n#{SPACER}"
+      end
+      sleep_time = interval - (Time.now - start)
+      boss.finished_work(@my_n, sleep_time)
+      break unless has_more
+      sleep(sleep_time) if sleep_time > 0.01
+    end
+    printlnq_with_time("DONE")
+    self
+  end
+end
+
+db = Db.new("pg").junk_data(RECORD_COUNT)
+s = Supervisor.new
+
+# this will be read from descriptor
+s.count = COLLECTOR_COUNT
+
+collectors = COLLECTOR_COUNT.times.map do |n|
+  Collector.new(n, db, s)
+end
+threads = collectors.map { |c| sleep(0.1) ; Thread.new() { c.run } }
+
+sleep(DURATION)
+
+s.count = 0
+collectors.map(&:please_shutdown)
+threads.each{ |t| t.join }
+puts
+
+collectors.map(&:run_status)
+db.run_status
